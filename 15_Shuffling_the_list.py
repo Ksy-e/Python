5c43f2042d6147b74b00845db0f7747b447ed4d9
Ksy-e <ksy-e@ya.ru>
2023-02-06 17:13:01 +0500

    15_Shuffling_the_list V2

--- 15_Shuffling_the_list.py
+++ 15_Shuffling_the_list.py
@@ -10,3 +10,16 @@ for i in range(len(my_list)-1, 0, -1):
     j = RI(0, i + 1)
     my_list[i], my_list[j] = my_list[j], my_list[i]
 print(f'Перемешанный список: {my_list}')
+
+#с использованием ускоренной обработки даных
+
+my_list1=[RI(0,100) for i in range(10)]
+print(f'Изначальный список: {my_list1}')
+
+def suff(my_list1):
+    for i in range(len(my_list1)-1, 0, -1):
+        j = RI(0, i + 1)
+        my_list1[i], my_list1[j] = my_list1[j], my_list1[i]
+
+my_list2=list(filter(suff(my_list1), my_list1))
+print(f'Перемешанный список: {my_list2}')
